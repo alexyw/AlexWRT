3f5ae71badc34ff1ada00846246408c1ec1a1420
alexyw <solomon9813@msn.com>
2026-05-22 16:47:32 +0800

    Refactor OpenWrt DIY script for configuration and plugins
    Updated the script to modify default settings, added new plugins, and removed some commented-out lines.

--- diy-part2-ss.sh
+++ diy-part2-ss.sh
@@ -1,46 +1,21 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
+# 经过 2026 现代化审计提纯版本
 
-# Modify default IP
+# 1. 修改默认管理 IP (保持你的习惯)
 sed -i 's/192.168.1.1/192.168.1.10/g' package/base-files/files/bin/config_generate
 
+# 2. 修改主机名为 AlexWRT
 sed -i "s/hostname='.*'/hostname='AlexWRT'/g" package/base-files/files/bin/config_generate
 
-#sirpdboy
-#git clone https://github.com/sirpdboy/sirpdboy-package.git package/sirpdboy-package
-git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
-git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
-git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
-git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
-git clone https://github.com/sirpdboy/luci-app-poweroffdevice.git package/luci-app-poweroffdevice
-git clone https://github.com/sirpdboy/luci-app-autotimeset.git package/luci-app-autotimeset
-
-#修正连接数（by ベ七秒鱼ベ）
+# 3. 修正最大并发连接数（提升多设备与挂机环境的 NAT 吞吐抗压能力）
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
 
-# themes添加（svn co 命令意思：指定版本如https://github）
-git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
-git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
-git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial
+# 4. 加入常用好评的第三方独立插件 (保留了体积小且仍维护的有用工具)
+# DNS 过滤去广告
 git clone https://github.com/kiddin9/luci-app-dnsfilter.git package/luci-app-dnsfilter
-
-#git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
-
-#添加额外软件包
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
-#git clone https://github.com/vernesong/OpenClash.git package/OpenClash
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-#git clone https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
+# IPTV 助手（如果是家庭内部网络且涉及 VLAN 配置，此插件依然有辅助价值）
 git clone https://github.com/riverscn/openwrt-iptvhelper.git package/openwrt-iptvhelper
-#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
-#添加smartdns
-git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+
+# 备注：AdGuardHome, SmartDNS 等大型插件建议直接在云端 make menuconfig 时
+# 从内置的 LuCI -> Applications 列表中勾选官方维护的版本，不要在此处外挂拉取。
